4b635b6d70480550f44774ad10ce20add4a03d69
gyam <gy-git@ha3.eu>
2019-04-27 13:52:41 +0200

    (feat) make camera follow transform

---
+++ Assets/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject camera;
+    private Vector3 cameraOffset;
+    private Vector3 cameraVelocity;
+    
+    void Start()
+    {
+        cameraVelocity = new Vector3();
+        cameraOffset = camera.transform.position - transform.position;
+        camera.transform.position = NextCameraPosition();
+    }
+
+    void Update()
+    {
+        camera.transform.position = MutateSmoothPosition();
+    }
+
+    private Vector3 MutateSmoothPosition()
+    {
+        return Vector3.SmoothDamp(
+            camera.transform.position,
+            NextCameraPosition(),
+            ref cameraVelocity,
+            0.4F,
+            10F,
+            Time.deltaTime
+        );
+    }
+
+    private Vector3 NextCameraPosition()
+    {
+        return transform.position + cameraOffset;
+    }
+}
