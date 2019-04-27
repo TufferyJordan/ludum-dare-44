552fd1a975fd309c5783c49cf90e651de77b5057
kcourtois <courtois.k2@gmail.com>
2019-04-27 10:59:33 +0200

    test spawner of platform

---
+++ Assets/SpawnerPlatform.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class SpawnerPlatform : MonoBehaviour
+{
+    public Object[] basicPlatform;
+    public Object[] obstaclePlatform;
+    public Object[] buildingPlatform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
