b2ddf42ba8925d46e287bd6692fd14ffb004f3a4
kcourtois <courtois.k2@gmail.com>
2019-04-27 14:16:55 +0200

    Added platform destroyer

---
+++ Assets/Scripts/DestroyerController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class DestroyerController : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(collision.gameObject);
+    }
+}
