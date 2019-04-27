9af5088faa35065d259d3f26f47f40f70eb7f487
gyam <gy-git@ha3.eu>
2019-04-27 13:51:52 +0200

    (feat) spawn new platforms as the transform moves

--- Assets/SpawnerPlatform.cs
+++ Assets/SpawnerPlatform.cs
@@ -6,28 +6,35 @@ using Random = UnityEngine.Random;
 
 public class SpawnerPlatform : MonoBehaviour
 {
+    private const int PlatformLength = 8;
+    private const int PlatformCountAhead = 5;
+    private const int MovementVelocity = 7;
+    
     public Object[] basicPlatform;
     public Object[] obstaclePlatform;
     public Object[] buildingPlatform;
 
-    private int currentLocation;
-    
-    
+    private int _nextSpawnLocation;
+    private List<GameObject> _spawned;
     
     // Start is called before the first frame update
     void Start()
     {
-        currentLocation = -1;
+        _nextSpawnLocation = -1;
+        
+        _spawned = new List<GameObject>();
 
-        var spawned = new List<GameObject>();
-
-        var PLATFORM_LENGTH = 8;
-        while (currentLocation < 4)
+        while (_nextSpawnLocation < PlatformCountAhead)
         {
-            var selectedPlatform = SelectRandomPlatform();
-            spawned.Add(Instantiate(selectedPlatform, new Vector3(currentLocation * PLATFORM_LENGTH, 0, 0), Quaternion.identity) as GameObject);
-            currentLocation++;
+            var selectedPlatform = RandomPlatformOf(basicPlatform);
+            _spawned.Add(CreateNewPlatform(selectedPlatform, _nextSpawnLocation));
+            _nextSpawnLocation++;
         }
+    }
+
+    private GameObject CreateNewPlatform(Object selectedPlatform, int location)
+    {
+        return Instantiate(selectedPlatform, new Vector3(location * PlatformLength, 0, 0), Quaternion.identity) as GameObject;
     }
 
     private Object SelectRandomPlatform()
@@ -35,19 +42,38 @@ public class SpawnerPlatform : MonoBehaviour
         var platforms = new[] {basicPlatform, obstaclePlatform, buildingPlatform};
         var platformType = RandomIntRange(0, 3);
         var currentPlatformType = platforms[platformType];
-        var selectedPlatform = currentPlatformType[RandomIntRange(0, currentPlatformType.Length)];
+        var selectedPlatform = RandomPlatformOf(currentPlatformType);
         return selectedPlatform;
+    }
+
+    private static Object RandomPlatformOf(Object[] currentPlatformType)
+    {
+        return currentPlatformType[RandomIntRange(0, currentPlatformType.Length)];
     }
 
     private static int RandomIntRange(int min, int max)
     {
-        var randomIntRange = (int)Math.Floor((double)Random.Range(min, max));
-        return randomIntRange;
+        return (int)Math.Floor((double)Random.Range(min, max));
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.position += new Vector3(MovementVelocity * Time.deltaTime, 0, 0);
+        if (ShouldSpawnNewPlatform())
+        {
+            SpawnNextPlatform();
+        }
+    }
+
+    private bool ShouldSpawnNewPlatform()
+    {
+        return transform.position.x - ((_nextSpawnLocation - PlatformCountAhead) * PlatformLength) > 0;
+    }
+    
+    private void SpawnNextPlatform()
+    {
+        var selectedPlatform = SelectRandomPlatform();
+        _spawned.Add(CreateNewPlatform(selectedPlatform, _nextSpawnLocation));
+        _nextSpawnLocation++;
     }
 }
