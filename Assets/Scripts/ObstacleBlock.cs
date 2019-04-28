a3e5671f43ac68338c2a678ab69a4c712c5dff58
gyam <gy-git@ha3.eu>
2019-04-28 19:44:03 +0200

    (feat) use real obstacle models

--- Assets/Scripts/ObstacleBlock.cs
+++ Assets/Scripts/ObstacleBlock.cs
@@ -1,20 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
 
 public class ObstacleBlock : MonoBehaviour
 {
+    public Object[] props;
+    
     private List<Transform> _deleteOnDestroy;
     
     void Start()
     {
         _deleteOnDestroy = new List<Transform>();
         
-        foreach (Transform prop in transform)
-        {
-            prop.SetParent(null);
-            _deleteOnDestroy.Add(prop);
-        }
+        var randomObstacle = props[Random.Range(0, props.Length)];
+
+        var invert = Random.Range(0, 2) == 0 ? 0 : 180;
+        var prop = Instantiate(randomObstacle, transform.position + new Vector3(0, 0, 0), Quaternion.Euler(0, Random.Range(-35, 35) + invert, 0)) as GameObject;
+
+        var physicalObject = prop.transform.GetChild(0).GetComponent<Collider>();
+        prop.transform.position += new Vector3(0, physicalObject.bounds.extents.y + 0.2F, 0);
+        
+        _deleteOnDestroy.Add(prop.transform);
     }
 
     void Update()
