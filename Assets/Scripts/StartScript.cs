94c326ad459d8c214f7bf64eb0d18d7265e570f1
Jordan Tuffery <tufferyjordan@gmail.com>
2019-04-29 11:11:55 +0200

    [START] finished Start scene

--- Assets/Scripts/StartScript.cs
+++ Assets/Scripts/StartScript.cs
@@ -1,13 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartScript : MonoBehaviour
 {
 
     public void OnStartClick()
     {
+        SceneManager.LoadScene(1);
+    }
 
+    public void OnCreditsClick()
+    {
+        // TODO: Credits Click
+    }
+
+    public void OnExitClick()
+    {
+        Application.Quit();
     }
 
 }
